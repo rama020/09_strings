36376152248ff941a80ea5f15ebd2fa62eca3172
rama020 <118764128+rama020@users.noreply.github.com>
2022-12-02 17:15:09 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,26 @@
+'''
+DATE:02TH NOV 2022
+DAY: FRIDAY
+TOPIC: STRINGS
+AUTHOR: RAMA BHARGAVI
+'''
+a="baru"
+print(a.endswith(''))
+print(a.endswith('  '))
+print(a.index("b"))
+print(a.index("u"))
+print(a.replace("b","r"))
+print(a.replace("r","m"))
+print(a.title())
+print(a.istitle())
+b="rama bhargavi"
+print(b.title())
+print(b.istitle())
+print(b.removeprefix('5'))
+print(b.index("h"))
+print(b.index("g"))
+print(b.index("i"))
+print(b.find("g"))
+print(b.find("s"))
+print(b.find("a"))
+print(b.find("h"))
